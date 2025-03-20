0e44f71b861fe380c8360e7ee04f478c0ef8627c
Rogerio Inacio <rogerio.reaper@gmail.com>
2025-03-20 16:41:22 -0300

    feat: add error entity

---
+++ entities/error.py
@@ -0,0 +1,9 @@
+class ScriptError:
+    def __init__(self, site: str, element: str, message: str, string_search: str):
+        self.site = site
+        self.element = element
+        self.message = message
+        self.string_search = string_search
+
+    def __str__(self):
+        return f"Site: {self.site}. Elemento: {self.element}. Mensagem: {self.message}. Busca: {self.string_search}"
