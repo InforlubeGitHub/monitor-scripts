ba5c6bee5147c2fe725337d9bf7f3cc532b0a767
Rogerio Inacio <rogerio.reaper@gmail.com>
2025-03-20 16:41:38 -0300

    refactor: change message

--- entities/message.py
+++ entities/message.py
@@ -1,28 +1,42 @@
 import json
 
+from entities.error import ScriptError
+
 
 class Message:
-    def __init__(self, service_name: str, content: str):
-        self.service_name = service_name
-        self.content = content
+    def __init__(self, error: ScriptError):
+        self.service_name = error.site
+        self.element = error.element
+        self.message = error.message
+        self.string_search = error.string_search
 
     def __str__(self):
         embed = {
-            "title": "Erro em monitoramento",
+            "title": f"Erro em serviço {self.service_name}",
             "description": "O site apresentou um erro durante o monitoramento.",
             "color": 7506394,
             "footer": {
-                "text": "Created by Inforlube",
+                "text": "Comunique-se com Rogerio Inacio em caso de problemas.",
             },
             "thumbnail": {
                 "url": "https://images.icon-icons.com/2871/PNG/512/bot_robot_insect_icon_181890.png"
             },
             "fields": [
                 {
-                    "name": "Site: " + self.service_name,
-                    "value": "Erro: " + self.content,
-                    "inline": True
+                    "name": "Busca",
+                    "value": self.string_search,
+                    "inline": False
                 },
+                {
+                    "name": "Elemento",
+                    "value": self.element,
+                    "inline": False
+                },
+                {
+                    "name": "Mensagem",
+                    "value": self.message,
+                    "inline": False
+                }
             ]
         }
 
