35ff113ae2dd776169de9b695261113124c4366b
Rogerio Inacio <rogerio.reaper@gmail.com>
2025-04-11 10:23:05 -0300

    feat: define retries for lubel

--- scripts/Lubel.py
+++ scripts/Lubel.py
@@ -10,31 +10,51 @@ from entities.error import ScriptError
 class Lubel:
     def __init__(self, driver):
         self.driver = driver
+        self.retries = 10
 
     def run(self):
         print("Iniciando busca no script Lubel")
         self.driver.get("https://www.moura.com.br/produtos/lubel")
 
-        try:
-            WebDriverWait(self.driver, 90).until(
-                EC.presence_of_element_located((By.CLASS_NAME, "cc-message-container")))
-            consent_button = self.driver.find_element(By.XPATH, "/html/body/div[2]/div[2]/a[2]")
+        for attempt in range(self.retries):
+            try:
+                WebDriverWait(self.driver, 90).until(
+                    EC.presence_of_element_located((By.CLASS_NAME, "cc-message-container")))
+                consent_button = self.driver.find_element(By.XPATH, "/html/body/div[2]/div[2]/a[2]")
 
-            consent_button.click()
-        except TimeoutException:
-            script_error = ScriptError("Lubel", "Cookie Dialog", "TimeoutException", "")
-            print(script_error)
-            return script_error
+                consent_button.click()
+                return None
+            except TimeoutException:
+                if attempt < self.retries:
+                    script_error = ScriptError("Lubel", "Cookie Dialog", "TimeoutException", "")
+                    print(script_error)
+                    return script_error
+                else:
+                    self.retries -= 1
 
-        know_oils_button = self.driver.find_element(By.CLASS_NAME, "chakra-button")
+        self.retries = 10
 
-        know_oils_button.click()
+        for attempt in range(self.retries):
+            try:
+                know_oils_button = self.driver.find_element(By.CLASS_NAME, "chakra-button")
 
-        catalog_pdf_button = self.driver.find_element(By.CLASS_NAME, "css-z8c6fa")
+                know_oils_button.click()
 
-        catalog_pdf_button.click()
+                catalog_pdf_button = self.driver.find_element(By.CLASS_NAME, "css-z8c6fa")
 
-        time.sleep(3)
+                catalog_pdf_button.click()
 
-        print("Script para Lubel finalizada com sucesso")
+                time.sleep(3)
+
+                print("Script para Lubel finalizada com sucesso")
+
+                return None
+
+            except Exception as e:
+                if attempt < self.retries:
+                    script_error = ScriptError("Lubel", "Catalog PDF", f"Exception não identificada. Detalhes: {e}", "")
+                    print(script_error)
+                    return script_error
+                else:
+                    self.retries -= 1
 
