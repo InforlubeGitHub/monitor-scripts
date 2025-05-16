e08b1c1f22179baece779a1434b3e8295fe042cb
Rogerio Inacio <rogerio.reaper@gmail.com>
2025-05-16 14:02:47 -0300

    feat: define runners

--- main.py
+++ main.py
@@ -4,13 +4,8 @@ import schedule
 import time
 
 from entities.config import Config
-from entities.error import ScriptError
-from entities.message import Message
 from notification.discord import Discord
-from scripts.Lubel import Lubel
-from scripts.Lubrax import Lubrax
-from scripts.Castrol import Castrol
-from scripts.TotalEnergies import TotalEnergies
+from scripts.Runner import Runner
 
 def main():
     print("Iniciando monitoramento...")
@@ -19,43 +14,34 @@ def main():
     config.load()
     config.show()
 
-    schedule.every(config.interval).minutes.do((lambda: run_scripts(config)))
+    discord_instance = Discord(config.discord_webhook)
 
-    run_scripts(config)
+    runners = []
+
+    lubel = Runner("Lubel", config.mobil_api_key, config.lubel_api_url, discord_instance)
+    castrol = Runner("Castrol", config.castrol_api_key, config.castrol_api_url, discord_instance)
+    total_energies = Runner("TotalEnergies", config.totalenergies_api_key, config.totalenergies_api_url, discord_instance)
+    mobil = Runner("Mobil", config.mobil_api_key, config.mobil_api_url, discord_instance)
+
+    runners.append(lubel)
+    runners.append(castrol)
+    runners.append(total_energies)
+    runners.append(mobil)
+
+    schedule.every(config.interval).minutes.do((lambda: run_scripts(runners)))
+
+    run_scripts(runners)
 
     while True:
         schedule.run_pending()
         time.sleep(1)
 
-def run_scripts(config: Config = None):
+def run_scripts(runners: list[Runner]) -> None:
     now = datetime.now()
     print(f"Rodando monitoramento as {now}")
 
-    discord_instance = Discord(
-        "https://discordapp.com/api/webhooks/1364673256631566510/V1RQwnSwAlyeZoKVn2YPW8kwII4ZlaeZRUHKexvFERn3-035lCOYM6mVqBstz5W6kDPX")
-
-    errors = []
-
-    scenarios = [
-        "Renault Duster 2020 2.0",
-        "Renault Fluence 2017",
-        "Honda Civic 2018 2.0"
-    ]
-
-    lubel = Lubel(config.lubel_api_key)
-
-    for scenario in scenarios:
-        try:
-            lubel.run(scenario)
-        except ScriptError as e:
-            errors.append(e)
-        except Exception as e:
-            script_error = ScriptError("Lubel", "Search", f"{e.__str__()}", "")
-            print(script_error)
-            errors.append(script_error)
-
-    for error in errors:
-        discord_instance.send(Message(error))
+    for runner in runners:
+        runner.run()
 
     print("Monitoramento finalizado com sucesso")
 
